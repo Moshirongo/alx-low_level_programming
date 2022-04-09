7709c8f6efe63699afef54708867edf87268cc56
Moshirongo <sospeterkelly@gmail.com>
2022-04-08 19:00:03 -0700

    print digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+
+	return (0);
+}
