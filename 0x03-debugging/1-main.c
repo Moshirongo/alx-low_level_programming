8766ce85c38549123b2a7162b66e3b43cd7152d3
Moshirongo <sospeterkelly@gmail.com>
2022-04-14 10:33:54 -0700

    dedbugging class alx

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints an infinite loop
+ *
+ * Return: 0 
+ */
+int main(void)
+{
+	int i;
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+       /**
+	* while (i < 10)
+	* {
+	* infinite loop - no increase of variable
+	* putchar(i);
+	* }
+	*/
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);	
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+repository for intro to debuging in C programming
