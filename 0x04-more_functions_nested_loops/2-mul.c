1ffe5127d69c8e4ff860aae2a2c47c69247963e1
Moshirongo <sospeterkelly@gmail.com>
2022-04-14 17:08:05 -0700

    multiplication of integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * mul multiplies two integers
+ * mul - multiplies two integers
  * @a: first integer
  * @b: second integer
- * return: Always 0
+ * Return: Always 0
  */
 int mul(int a, int b)
 {
